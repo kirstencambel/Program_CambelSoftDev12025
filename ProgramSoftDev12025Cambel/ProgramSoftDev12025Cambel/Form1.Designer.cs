87392f49a61de067c5b7fc3665d123ac1635bc41
kirstencambel <cambelkirsten1@gmail.com>
2025-10-03 12:22:30 +0800

    Add files via upload

---
+++ ProgramSoftDev12025Cambel/ProgramSoftDev12025Cambel/Form1.Designer.cs
@@ -0,0 +1,372 @@
+﻿namespace ProgramSoftDev12025Cambel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUncheckAll = new System.Windows.Forms.Button();
+            this.btnCheckAll = new System.Windows.Forms.Button();
+            this.btnAddNewRecord = new System.Windows.Forms.Button();
+            this.DGV = new System.Windows.Forms.DataGridView();
+            this.tblProgramBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.softDev1DBDataSet = new ProgramSoftDev12025Cambel.SoftDev1DBDataSet();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtCollegeID = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtCollegeCode = new System.Windows.Forms.TextBox();
+            this.txtCollegeName = new System.Windows.Forms.TextBox();
+            this.tbl_ProgramTableAdapter = new ProgramSoftDev12025Cambel.SoftDev1DBDataSetTableAdapters.tbl_ProgramTableAdapter();
+            this.Select = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.RowNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.programIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descriptionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.codeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.collegeIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.deletedDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Action = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblProgramBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.softDev1DBDataSet)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnDelete);
+            this.groupBox1.Controls.Add(this.btnUncheckAll);
+            this.groupBox1.Controls.Add(this.btnCheckAll);
+            this.groupBox1.Controls.Add(this.btnAddNewRecord);
+            this.groupBox1.Controls.Add(this.DGV);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(845, 360);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(632, 40);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(186, 38);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUncheckAll
+            // 
+            this.btnUncheckAll.Location = new System.Drawing.Point(428, 38);
+            this.btnUncheckAll.Name = "btnUncheckAll";
+            this.btnUncheckAll.Size = new System.Drawing.Size(198, 39);
+            this.btnUncheckAll.TabIndex = 7;
+            this.btnUncheckAll.Text = "Uncheck All";
+            this.btnUncheckAll.UseVisualStyleBackColor = true;
+            this.btnUncheckAll.Click += new System.EventHandler(this.btnUncheckAll_Click);
+            // 
+            // btnCheckAll
+            // 
+            this.btnCheckAll.Location = new System.Drawing.Point(226, 40);
+            this.btnCheckAll.Name = "btnCheckAll";
+            this.btnCheckAll.Size = new System.Drawing.Size(196, 38);
+            this.btnCheckAll.TabIndex = 6;
+            this.btnCheckAll.Text = "Check All";
+            this.btnCheckAll.UseVisualStyleBackColor = true;
+            this.btnCheckAll.Click += new System.EventHandler(this.btnCheckAll_Click);
+            // 
+            // btnAddNewRecord
+            // 
+            this.btnAddNewRecord.Location = new System.Drawing.Point(11, 39);
+            this.btnAddNewRecord.Name = "btnAddNewRecord";
+            this.btnAddNewRecord.Size = new System.Drawing.Size(209, 39);
+            this.btnAddNewRecord.TabIndex = 5;
+            this.btnAddNewRecord.Text = "Add New Record";
+            this.btnAddNewRecord.UseVisualStyleBackColor = true;
+            this.btnAddNewRecord.Click += new System.EventHandler(this.btnAddNewRecord_Click);
+            // 
+            // DGV
+            // 
+            this.DGV.AllowUserToAddRows = false;
+            this.DGV.AllowUserToDeleteRows = false;
+            this.DGV.AutoGenerateColumns = false;
+            this.DGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Select,
+            this.RowNumber,
+            this.programIDDataGridViewTextBoxColumn,
+            this.descriptionDataGridViewTextBoxColumn,
+            this.codeDataGridViewTextBoxColumn,
+            this.collegeIDDataGridViewTextBoxColumn,
+            this.deletedDataGridViewTextBoxColumn,
+            this.Action});
+            this.DGV.DataSource = this.tblProgramBindingSource;
+            this.DGV.Location = new System.Drawing.Point(11, 84);
+            this.DGV.Name = "DGV";
+            this.DGV.ReadOnly = true;
+            this.DGV.RowHeadersVisible = false;
+            this.DGV.Size = new System.Drawing.Size(807, 257);
+            this.DGV.TabIndex = 1;
+            this.DGV.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DGV_CellClick);
+            // 
+            // tblProgramBindingSource
+            // 
+            this.tblProgramBindingSource.DataMember = "tbl_Program";
+            this.tblProgramBindingSource.DataSource = this.softDev1DBDataSet;
+            // 
+            // softDev1DBDataSet
+            // 
+            this.softDev1DBDataSet.DataSetName = "SoftDev1DBDataSet";
+            this.softDev1DBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtCollegeID);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.btnCancel);
+            this.groupBox2.Controls.Add(this.btnClear);
+            this.groupBox2.Controls.Add(this.btnSave);
+            this.groupBox2.Controls.Add(this.txtCollegeCode);
+            this.groupBox2.Controls.Add(this.txtCollegeName);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(23, 370);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(764, 398);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Add New College";
+            // 
+            // txtCollegeID
+            // 
+            this.txtCollegeID.Location = new System.Drawing.Point(167, 128);
+            this.txtCollegeID.Name = "txtCollegeID";
+            this.txtCollegeID.Size = new System.Drawing.Size(343, 29);
+            this.txtCollegeID.TabIndex = 7;
+            this.txtCollegeID.TextChanged += new System.EventHandler(this.txtCollegeID_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "College ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "College Code";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "College Name";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(400, 171);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(110, 33);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(285, 171);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(109, 32);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(167, 171);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(112, 32);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtCollegeCode
+            // 
+            this.txtCollegeCode.Location = new System.Drawing.Point(167, 90);
+            this.txtCollegeCode.Name = "txtCollegeCode";
+            this.txtCollegeCode.Size = new System.Drawing.Size(343, 29);
+            this.txtCollegeCode.TabIndex = 1;
+            // 
+            // txtCollegeName
+            // 
+            this.txtCollegeName.Location = new System.Drawing.Point(167, 55);
+            this.txtCollegeName.Name = "txtCollegeName";
+            this.txtCollegeName.Size = new System.Drawing.Size(343, 29);
+            this.txtCollegeName.TabIndex = 0;
+            // 
+            // tbl_ProgramTableAdapter
+            // 
+            this.tbl_ProgramTableAdapter.ClearBeforeFill = true;
+            // 
+            // Select
+            // 
+            this.Select.HeaderText = "Select";
+            this.Select.Name = "Select";
+            this.Select.ReadOnly = true;
+            // 
+            // RowNumber
+            // 
+            this.RowNumber.DataPropertyName = "programID";
+            this.RowNumber.HeaderText = "Row No.";
+            this.RowNumber.Name = "RowNumber";
+            this.RowNumber.ReadOnly = true;
+            this.RowNumber.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.RowNumber.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // programIDDataGridViewTextBoxColumn
+            // 
+            this.programIDDataGridViewTextBoxColumn.DataPropertyName = "programID";
+            this.programIDDataGridViewTextBoxColumn.HeaderText = "programID";
+            this.programIDDataGridViewTextBoxColumn.Name = "programIDDataGridViewTextBoxColumn";
+            this.programIDDataGridViewTextBoxColumn.ReadOnly = true;
+            this.programIDDataGridViewTextBoxColumn.Visible = false;
+            // 
+            // descriptionDataGridViewTextBoxColumn
+            // 
+            this.descriptionDataGridViewTextBoxColumn.DataPropertyName = "description";
+            this.descriptionDataGridViewTextBoxColumn.HeaderText = "College Name";
+            this.descriptionDataGridViewTextBoxColumn.Name = "descriptionDataGridViewTextBoxColumn";
+            this.descriptionDataGridViewTextBoxColumn.ReadOnly = true;
+            this.descriptionDataGridViewTextBoxColumn.Width = 250;
+            // 
+            // codeDataGridViewTextBoxColumn
+            // 
+            this.codeDataGridViewTextBoxColumn.DataPropertyName = "code";
+            this.codeDataGridViewTextBoxColumn.HeaderText = "College Code";
+            this.codeDataGridViewTextBoxColumn.Name = "codeDataGridViewTextBoxColumn";
+            this.codeDataGridViewTextBoxColumn.ReadOnly = true;
+            this.codeDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // collegeIDDataGridViewTextBoxColumn
+            // 
+            this.collegeIDDataGridViewTextBoxColumn.DataPropertyName = "collegeID";
+            this.collegeIDDataGridViewTextBoxColumn.HeaderText = "collegeID";
+            this.collegeIDDataGridViewTextBoxColumn.Name = "collegeIDDataGridViewTextBoxColumn";
+            this.collegeIDDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // deletedDataGridViewTextBoxColumn
+            // 
+            this.deletedDataGridViewTextBoxColumn.DataPropertyName = "deleted";
+            this.deletedDataGridViewTextBoxColumn.HeaderText = "deleted";
+            this.deletedDataGridViewTextBoxColumn.Name = "deletedDataGridViewTextBoxColumn";
+            this.deletedDataGridViewTextBoxColumn.ReadOnly = true;
+            this.deletedDataGridViewTextBoxColumn.Visible = false;
+            // 
+            // Action
+            // 
+            this.Action.DataPropertyName = "programID";
+            this.Action.HeaderText = "Action";
+            this.Action.Name = "Action";
+            this.Action.ReadOnly = true;
+            this.Action.Text = "Edit";
+            this.Action.UseColumnTextForButtonValue = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(871, 370);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Enter += new System.EventHandler(this.SS);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DGV)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblProgramBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.softDev1DBDataSet)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView DGV;
+        private SoftDev1DBDataSet softDev1DBDataSet;
+        private System.Windows.Forms.BindingSource tblProgramBindingSource;
+        private SoftDev1DBDataSetTableAdapters.tbl_ProgramTableAdapter tbl_ProgramTableAdapter;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUncheckAll;
+        private System.Windows.Forms.Button btnCheckAll;
+        private System.Windows.Forms.Button btnAddNewRecord;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtCollegeCode;
+        private System.Windows.Forms.TextBox txtCollegeName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCollegeID;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Select;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RowNumber;
+        private System.Windows.Forms.DataGridViewTextBoxColumn programIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descriptionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn codeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn collegeIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn deletedDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewButtonColumn Action;
+    }
+}
+
---
+++ ProgramSoftDev12025Cambel/ProgramSoftDev12025Cambel/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgramSoftDev12025Cambel
+{
+    public partial class Form1 : Form
+    {
+        int collegeID, SaveFlag, programID;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            groupBox1.Location = new Point(12, 12);
+            groupBox2.Location = new Point(12, 12);
+            ResetAll();
+            this.tbl_ProgramTableAdapter.Fill(this.softDev1DBDataSet.tbl_Program);
+        }
+
+
+        private void txtCollegeID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddNewRecord_Click(object sender, EventArgs e)
+        {
+            groupBox1.Visible = false;
+            groupBox2.Visible = true;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtCollegeName.Text = string.Empty;
+            txtCollegeCode.Text = string.Empty;
+            txtCollegeID.Text = string.Empty;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            {
+                switch (SaveFlag)
+                {
+                    case 0: //Save
+                        try
+                        {
+                            int.TryParse(txtCollegeID.Text, out collegeID);
+                            this.tbl_ProgramTableAdapter.InsertQuery(txtCollegeName.Text, txtCollegeCode.Text, collegeID, 0);
+                            MessageBox.Show("New Record Added");
+                            ResetAll();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                            ResetAll();
+                        }
+                        break;
+                    case 1: //Action/Edit
+                        MessageBox.Show("Record Updated");
+                        int.TryParse(txtCollegeID.Text, out collegeID);
+                        this.tbl_ProgramTableAdapter.UpdateQuery(txtCollegeName.Text, txtCollegeCode.Text, collegeID, 0, programID);
+                        ResetAll();
+                        break;
+                }
+                this.tbl_ProgramTableAdapter.Fill(this.softDev1DBDataSet.tbl_Program);
+            }
+
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            ResetAll();
+        }
+        public void ResetAll()
+        {
+            groupBox1.Visible = true;
+            groupBox2.Visible = false;
+            txtCollegeName.Text = string.Empty;
+            txtCollegeCode.Text = string.Empty;
+            txtCollegeID.Text = string.Empty;
+            this.tbl_ProgramTableAdapter.Fill(this.softDev1DBDataSet.tbl_Program);
+        }
+
+        public void CheckUncheckAll(bool x)
+        {
+            int i = 0;
+            while (i < DGV.RowCount)
+            {
+                DGV.Rows[i].Cells[0].Value = x;
+                i++;
+            }
+
+        }
+
+        private void DGV_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int i = e.RowIndex;
+            int j = e.ColumnIndex;
+
+            //Select Column
+            if (j == 0 && i >= 0)
+            {
+                if (Convert.ToBoolean(DGV[0, i].Value) == false)
+                {
+                    DGV[0, i].Value = true;
+                }
+                else
+                {
+                    DGV[0, i].Value = false;
+                }
+            }
+
+            //Action Column
+            if (j == 7 && i >= 0)
+            {
+                groupBox1.Visible = false;
+                groupBox2.Visible = true;
+
+                programID = Convert.ToInt16(DGV[2, i].Value.ToString());
+                txtCollegeName.Text = DGV[3, i].Value.ToString();
+                txtCollegeCode.Text = DGV[4, i].Value.ToString();
+                txtCollegeID.Text = DGV[5, i].Value.ToString();
+                SaveFlag = 1;
+            }
+        }
+
+        private void btnCheckAll_Click(object sender, EventArgs e)
+        {
+            CheckUncheckAll(true);
+        }
+
+        private void btnUncheckAll_Click(object sender, EventArgs e)
+        {
+            CheckUncheckAll(false);
+        }
+
+        private void SS(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Confirm Record/s Delete", "Warning", MessageBoxButtons.YesNo);
+            switch (result)
+            {
+                case DialogResult.Yes:
+                    int i = 0;
+                    int j = 0;
+
+                    while (i < DGV.RowCount)
+                    {
+                        if (Convert.ToBoolean(DGV[0, i].Value) == true)
+                        {
+                            try
+                            {
+                                this.tbl_ProgramTableAdapter.DeleteQuery((int)DGV[2, i].Value);
+
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show(ex.Message);
+                            }
+                            j++;
+                            i++;
+                        }
+                        else
+                        {
+                            i++;
+                        }
+                    }
+                    MessageBox.Show(j + " record/s deleted");
+                    this.tbl_ProgramTableAdapter.Fill(this.softDev1DBDataSet.tbl_Program);
+                    break;
+                case DialogResult.No:
+                    //DO NOTHING
+                    break;
+            }
+        }
+
+        private void DGV_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            int i = 0;
+            while (i < DGV.RowCount)
+            {
+                DGV.Rows[i].Cells[1].Value = i + 1;
+                i++;
+            }
+        }
+    }
+
+    }
